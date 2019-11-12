90d65ea724b4704e60d0992433e16879f3f4cf15
chenzheng311 <1686505293@qq.com>
2019-11-12 09:23:14 +0800

    wdfwefwe

---
+++ ctest12.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+int main(void)
+{
+    int x, y;
+    printf("输入成绩\n");
+    scanf("%d", &x);
+    if (x >= 100)
+    {
+        y = -1;
+    }
+    else if (x >= 90)
+    {
+        y = 4;
+    }
+    else if (x >= 80)
+    {
+        y = 3;
+    }
+    else if (x >= 70)
+    {
+        y = 2;
+    }
+    else if (x >= 60)
+    {
+        y = 1;
+    }
+    else
+        y = 0;
+    switch (y)
+    {
+    case 4:
+        printf("excellent\n");
+        break;
+    case 3:
+        printf("good\n");
+        break;
+    case 2:
+        printf("average\n");
+        break;
+    case 1:
+        printf("poor\n");
+        break;
+    case 0:
+        printf("failing\n");
+        break;
+    default:
+        printf("illegal\n");
+        break;
+    }
+    return 0;
+}
