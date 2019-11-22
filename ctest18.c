7ed9863efe64f551d31ed6111fee71eb805576e8
chenzheng311 <1686505293@qq.com>
2019-11-22 09:28:44 +0800

    qwdwqd

--- ctest18.c
+++ ctest18.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
-int maun(void)
+int main(void)
 {
-    
+    int sum = 0, x;
+    float m;
+    printf("Enter:");
+    scanf("%d", &x); 
+    while (x>0)
+    {
+        m = x % 10;
+        sum += m;
+        x = x / 10;
+    }
+    printf("sum=%d", sum);
     return 0;
 }
