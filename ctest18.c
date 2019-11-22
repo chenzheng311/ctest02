96da85a8934e697e35bbe0e3d9883d50d12588b2
chenzheng311 <1686505293@qq.com>
2019-11-22 09:39:43 +0800

    laknlnafna

--- ctest18.c
+++ ctest18.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 int main(void)
 {
-    int sum = 0, x;
+    int sum = 0, x,a=0;
     float m;
     printf("Enter:");
     scanf("%d", &x); 
     while (x>0)
     {
         m = x % 10;
-        sum += m;
+        sum += m*m*m;
         x = x / 10;
+        a=a+1;
     }
-    printf("sum=%d", sum);
+    printf("sum=%d,a=%d", sum,a);
     return 0;
 }
